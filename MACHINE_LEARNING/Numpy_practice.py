564772aae4fa1140fb7bd896a3fafbc1d46f3a87
Manish Kumar <manishraj.29111998@gmail.com>
2021-10-16 01:08:29 +0530

    Machine Learning -part1

---
+++ MACHINE_LEARNING/Numpy_practice.py
@@ -0,0 +1,29 @@
+import numpy as np
+# array1=np.arange(1,15,1)
+# print(array1)
+
+# array1=np.arange(1,15,2)
+# print(array1)
+
+# array1=np.arange(1,19,2).reshape(3,3)
+# print(array1)
+
+# array1=np.linspace(1,10,10)
+# print(array1)
+
+# array1=np.logspace(1,20,10)
+# print(array1)
+# array1=np.array([1,2,3,4,5])
+# array2=np.array([6,7,8,9,10])
+# print(array1)
+# print(array2)
+# print(np.ndim(array1))
+# print(np.shape(array1))
+# print(np.size(array1))
+# print(np.hstack((array1,array2)))
+# print(np.vstack((array1,array2)))
+
+# array1=np.linspace(1,10,10)
+# for x in np.nditer(array1):
+#     print(x)
+
